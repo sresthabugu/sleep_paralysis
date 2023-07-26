bdb15177225595dd3aa3d60d647e3a74afbb72bd
SresthSaha <108623720+sresthabugu@users.noreply.github.com>
2023-07-26 20:30:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+window.addEventListener('scroll', () => {
+    let value = window.scrollY;
+    text.style.marginTop = value * 2.5 + 'px';
+    treeLeft.style.left = value * -1.5 + 'px';
+    treeRight.style.left = value * 1.5 + 'px';
+
+    gateLeft.style.left = value * -.75 + 'px';
+    gateRight.style.left= value * .75 + 'px';
+    grass.style.marginTop= value * -.2 + 'px';
+
+
+});
+
+// Replace text, treeLeft, and treeRight with the actual element references
+let text = document.getElementById('text');
+let treeLeft = document.getElementById('tree-left');
+let treeRight = document.getElementById('tree-right');
+let gateLeft = document.getElementById('gate-left');
+let gateRight = document.getElementById('gate-right');
+let grass = document.getElementById('grass');
+
+
+  
